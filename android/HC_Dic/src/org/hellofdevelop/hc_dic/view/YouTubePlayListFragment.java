482128170de5b921451b7059b904fda074e559e2
wonjai <wonjai@wonjaiui-iMac.local>
2014-11-03 21:56:11 +0900

    * 유튜브 프래그먼트 추가

--- android/HC_Dic/src/org/hellofdevelop/hc_dic/view/YouTubePlayListFragment.java
+++ android/HC_Dic/src/org/hellofdevelop/hc_dic/view/YouTubePlayListFragment.java
@@ -2,6 +2,7 @@ package org.hellofdevelop.hc_dic.view;
 
 import org.hellofdevelop.hc_dic.Const;
 import org.hellofdevelop.hc_dic.R;
+import org.json.JSONObject;
 
 import android.os.Bundle;
 import android.util.Log;
@@ -10,6 +11,8 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.androidquery.AQuery;
+import com.androidquery.callback.AjaxCallback;
+import com.androidquery.callback.AjaxStatus;
 
 public class YouTubePlayListFragment extends BaseFragment {
 	
@@ -49,6 +52,30 @@ public class YouTubePlayListFragment extends BaseFragment {
 		super.onResume();
 
 		//super.startLoader(kLOADER_ID_HEROES, null, this);
+		{
+			String baseUrl = "https://www.googleapis.com/youtube/v3";
+			String apiKey = "AIzaSyB0ie-L57C9wCyE8fuVbJSiXU_G2w44BrI";
+			String playlistId = "AIzaSyB0ie-L57C9wCyE8fuVbJSiXU_G2w44BrI";
+			
+			String uriString = String.format("%s/playlistItems?part=contentDetails&playlistId=%s&key=%s", baseUrl, playlistId, apiKey);
+
+			mAQuery.ajax(uriString, JSONObject.class, new AjaxCallback<JSONObject>() {
+
+				@Override
+				public void callback(String url, JSONObject object,
+						AjaxStatus status) {
+					if (status.getCode() != 200) {
+						Log.e(TAG, String.format("code=%d", status.getCode()));
+						
+						return;
+					}
+					
+					if (DEBUG) Log.d(TAG, object.toString());
+					
+				}
+				
+			});
+		}
 	}
 
 	@Override
